2293e8255f5277088a1c0d4a36db55435a29eba7
Marc Kirsch <marc.kirsch17@gmail.com>
2018-11-16 21:51:38 -0600

    a lot of stuff

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+var friends = [{
+    name: "Saquon Barkley",
+    photo: "https://goo.gl/images/wNB4Jk",
+    scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
+  }, {
+    name: "Eli Manning",
+    photo: "https://goo.gl/images/SAmAoe",
+    scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
+  }, {
+    name: "Odell Beckham Jr",
+    photo: "https://goo.gl/images/6Lo5X4",
+    scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+  },{
+    name: "Kyrie Irving",
+    photo: "https://goo.gl/images/8XRLEC",
+    scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+  }, {
+    name: "Jayson Tatum",
+    photo: "https://goo.gl/images/BWyB3W",
+    scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+  }, {
+    name: "Gordon Hayward",
+    photo: "https://goo.gl/images/BQakBc",
+    scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+  }, {
+    name: "Al Horford",
+    photo: "https://goo.gl/images/p1qsKf",
+    scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+  }];
+  
+ module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,47 @@
+var friendData = require('../data/friends.js');
+var path = require('path');
+
+
+module.exports = function(app){
+
+	app.get('/api/theList', function(req, res){
+		res.json(friendData);
+	});
+
+
+	app.post('/api/theList', function(req, res){
+		var lowestDifferenceInt = 50;
+		var chosenMatch;
+		friendData.forEach(function(storedUserObject){
+			var difference = 0;
+			for(i=0;i<storedUserObject.friendNumbers.length;i++){
+				difference+=Math.abs(storedUserObject.friendNumbers[i] - req.body.friendNumbers[i]);
+			} if(difference<lowestDifferenceInt){
+				lowestDifferenceInt = difference;
+				chosenMatch = storedUserObject;
+			}
+		});
+
+
+		res.json(chosenMatch);
+		friendData.push(req.body);
+
+/*
+		if(tableData.length < 5 ){
+			tableData.push(req.body);
+			res.json(true); // KEY LINE
+		}
+		// Or false if they don't have a table
+		else{
+			waitListData.push(req.body);
+			res.json(false); // KEY LINE
+		}
+*/
+	});
+
+	app.post('/api/clear', function(req, res){
+		friendData = [];
+
+		console.log(friendData);
+	})
+}
